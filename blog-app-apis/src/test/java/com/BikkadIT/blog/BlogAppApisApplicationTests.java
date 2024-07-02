bf2948970f25e68c00c6b7f5cafbd4d4e21c2162
vishnusolankar <solankarvish>
2024-07-02 18:25:25 +0530

    Add test cases for UserRepo class in BlogAppApisApplicationTests !!

--- blog-app-apis/src/test/java/com/BikkadIT/blog/BlogAppApisApplicationTests.java
+++ blog-app-apis/src/test/java/com/BikkadIT/blog/BlogAppApisApplicationTests.java
@@ -6,23 +6,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.BikkadIT.blog.repositories.UserRepo;
 
-@SpringBootTest
+@SpringBootTest // Annotation to indicate that this is a Spring Boot test class
 class BlogAppApisApplicationTests {
 
-	@Autowired
+	@Autowired // Autowires the UserRepo bean for dependency injection
 	private UserRepo userRepo;
 
-	@Test
+	@Test // A simple test method to check if the context loads successfully
 	void contextLoads() {
+		// This test will pass if the application context loads without any issues
 	}
 
-	@Test
-	public void repoTest() 
-	{
+	@Test // A test method to check the class name and package name of the UserRepo bean
+	public void repoTest() {
+		// Get the class name of the UserRepo bean
 		String className = this.userRepo.getClass().getName();
+		// Get the package name of the UserRepo bean
 		String packName = this.userRepo.getClass().getPackageName();
+		// Print the class name to the console
 		System.out.println(className);
+		// Print the package name to the console
 		System.out.println(packName);
 	}
-
 }
